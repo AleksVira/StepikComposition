ef3370941c74a8d0fa0b683389ba98ed787c38ed
aleksvira <aleksvira@yandex.ru>
2025-06-07 17:58:38 +0300

    Fragment Navigation

--- app/src/main/java/ru/virarnd/stepcomposition/presentation/ChooseLevelFragment.kt
+++ app/src/main/java/ru/virarnd/stepcomposition/presentation/ChooseLevelFragment.kt
@@ -6,14 +6,37 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import java.lang.RuntimeException
+import ru.virarnd.stepcomposition.R
 import ru.virarnd.stepcomposition.databinding.FragmentChooseLevelBinding
+import ru.virarnd.stepcomposition.domain.entity.Level
 
 class ChooseLevelFragment : Fragment() {
+
+    companion object {
+
+        const val CHOOSE_LEVEL_FRAGMENT_NAME = "ChooseLevelFragment"
+
+        fun newInstance(): ChooseLevelFragment {
+            return ChooseLevelFragment()
+        }
+    }
 
     private var _binding: FragmentChooseLevelBinding? = null
     private val binding: FragmentChooseLevelBinding
         get() = _binding ?: throw RuntimeException("FragmentChooseLevelBinding == null")
 
+    private val levelButtonClickListener: View.OnClickListener = View.OnClickListener {
+        val gameLevel = when(it.id) {
+            R.id.button_level_easy -> Level.EASY
+            R.id.button_level_normal -> Level.NORMAL
+            R.id.button_level_hard -> Level.HARD
+            else -> Level.TEST
+        }
+        requireActivity().supportFragmentManager.beginTransaction()
+            .replace(R.id.main_container, GameFragment.newInstance(gameLevel))
+            .addToBackStack(GameFragment.GAME_FRAGMENT_NAME)
+            .commit()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,9 +46,18 @@ class ChooseLevelFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        with(binding){
+            buttonLevelTest.setOnClickListener(levelButtonClickListener)
+            buttonLevelEasy.setOnClickListener(levelButtonClickListener)
+            buttonLevelNormal.setOnClickListener(levelButtonClickListener)
+            buttonLevelHard.setOnClickListener(levelButtonClickListener)
+        }
+    }
 
-    override fun onDestroy() {
-        super.onDestroy()
+    override fun onDestroyView() {
+        super.onDestroyView()
         _binding = null
     }
 
--- app/src/main/java/ru/virarnd/stepcomposition/presentation/GameFinishedFragment.kt
+++ app/src/main/java/ru/virarnd/stepcomposition/presentation/GameFinishedFragment.kt
@@ -1,19 +1,43 @@
 package ru.virarnd.stepcomposition.presentation
 
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import java.lang.RuntimeException
 import ru.virarnd.stepcomposition.databinding.FragmentGameFinishedBinding
+import ru.virarnd.stepcomposition.domain.entity.GameResult
+import ru.virarnd.stepcomposition.presentation.GameFragment.Companion.GAME_FRAGMENT_NAME
 
 class GameFinishedFragment : Fragment() {
+
+    companion object {
+
+        private const val KEY_RESULT = "gameResult"
+
+        fun newInstance(gameResult: GameResult) : GameFinishedFragment {
+            return GameFinishedFragment().apply {
+                arguments = Bundle().apply {
+                    putSerializable(KEY_RESULT, gameResult)
+                }
+            }
+        }
+    }
 
     private var _binding: FragmentGameFinishedBinding? = null
     private val binding: FragmentGameFinishedBinding
         get() = _binding ?: throw RuntimeException("FragmentGameFinishedBinding == null")
 
+    private lateinit var gameResult: GameResult
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        parseArgs()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,11 +47,32 @@ class GameFinishedFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner, object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                retryGame()
+            }
+        })
+    }
 
 
-    override fun onDestroy() {
-        super.onDestroy()
+    override fun onDestroyView() {
+        super.onDestroyView()
         _binding = null
+    }
+
+    private fun parseArgs() {
+        gameResult = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            requireArguments().getSerializable(KEY_RESULT, GameResult::class.java)
+        } else {
+            arguments?.getSerializable(KEY_RESULT) as GameResult
+        } ?: throw RuntimeException("GameResult is null")
+    }
+
+
+    private fun retryGame() {
+        requireActivity().supportFragmentManager.popBackStack(GAME_FRAGMENT_NAME, FragmentManager.POP_BACK_STACK_INCLUSIVE)
     }
 
 }
